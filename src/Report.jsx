285ff9b83e1235a599368cda5d22ea0bf88c8190
safwanmoha <safwan.mohammed@trycake.com>
2017-11-30 12:50:55 +0530

    reports UI

--- src/Report.jsx
+++ src/Report.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import jquery from 'jquery'
 
 export default class Examples extends Component {
   constructor(props) {
@@ -6,8 +7,20 @@ export default class Examples extends Component {
   }
   render() {
     return (
-	    <div>
-	    </div>
+      < BrowserRouter>
+    <div className='container'>
+    <div className="page-header">
+        <h1>Send Report</h1>
+      </div>
+      <form >
+        <table>
+          <tr><th>Please type the error: </th> <textarea name="name" id="name"/></tr>
+          <tr><th></th> <th><input type='button' name="Send" value="Send"/></th></tr>
+        </table>
+      </form>
+      <Link to="/">Cancel</Link>
+    </div>
+  </ BrowserRouter>
     );
   }
 }
